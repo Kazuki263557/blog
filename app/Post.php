a2847162a77d17b224b3e05466e0038970702ea0
Kazuki <okuno263557@gmail.com>
2022-10-15 01:19:11 +0900

    アプリケーションの開発

--- app/Post.php
+++ app/Post.php
@@ -1,16 +1,10 @@
 <?php
 
-namespace App;
+namespace App\Http\Controllers;
 
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Http\Request;
 
-class Post extends Model
+class PostController extends Controller
 {
     //
-}
-
-public function getByLimit(int $limit_count = 10)
-{
-    // updated_atで降順に並べたあと、limitで件数制限をかける
-    return $this->orderBy('updated_at', 'DESC')->limit($limit_count)->get();
 }
