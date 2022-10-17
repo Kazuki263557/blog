ad8c1cb622cdbd7bb700a714b7eea23a622914dd
Kazuki <okuno263557@gmail.com>
2022-10-17 18:03:36 +0900

    ブログ投稿詳細画面実装

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -6,9 +6,16 @@ use App\Post;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
+
 {
-    public function index(Post $post)
-    {
-         return $post->get();
-    }
+public function index(Post $post)
+{
+    return view('posts/index')->with(['posts' => $post->getPaginateByLimit()]);
+} 
+public function show(Post $post)
+{
+    return view('posts/show')->with(['post' => $post]);
 }
+
+}
+?>
--- app/Post.php
+++ app/Post.php
@@ -1,10 +1,23 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App;
 
-use Illuminate\Http\Request;
+use Illuminate\Database\Eloquent\Model;
 
-class PostController extends Controller
+class Post extends Model
+
 {
-    //
+    public function getByLimit(int $limit_count = 10)
+
+  { 
+    // updated_atで降順に並べたあと、limitで件数制限をかける
+    return $this->orderBy('updated_at', 'DESC')->limit($limit_count)->get();
+  }
+    
+   public function getPaginateByLimit(int $limit_count = 10)
+  {
+    // updated_atで降順に並べたあと、limitで件数制限をかける
+    return $this->orderBy('updated_at', 'DESC')->paginate($limit_count);
+  }
+  
 }
